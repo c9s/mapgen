9cfcca0429bd5f1c27d7927be14c9ca74e658976
c9s <yoanlin93@gmail.com>
2024-11-29 01:11:58 +0800

    first comit

---
+++ testdata/privateevent.go
@@ -0,0 +1,29 @@
+package testdata
+
+type PrivateChannel string
+
+const (
+	PrivateChannelOrder           PrivateChannel = "order"
+	PrivateChannelOrderUpdate     PrivateChannel = "order_update"
+	PrivateChannelTrade           PrivateChannel = "trade"
+	PrivateChannelTradeUpdate     PrivateChannel = "trade_update"
+	PrivateChannelTradeFastUpdate PrivateChannel = "trade_fast_update"
+	PrivateChannelAccount         PrivateChannel = "account"
+	PrivateChannelAccountUpdate   PrivateChannel = "account_update"
+
+	// @group Margin
+	PrivateChannelMWalletOrder           PrivateChannel = "mwallet_order"
+	PrivateChannelMWalletTrade           PrivateChannel = "mwallet_trade"
+	PrivateChannelMWalletTradeFastUpdate PrivateChannel = "mwallet_trade_fast_update"
+	PrivateChannelMWalletAccount         PrivateChannel = "mwallet_account"
+	PrivateChannelMWalletAveragePrice    PrivateChannel = "mwallet_average_price"
+	PrivateChannelBorrowing              PrivateChannel = "borrowing"
+	PrivateChannelAdRatio                PrivateChannel = "ad_ratio"
+	PrivateChannelPoolQuota              PrivateChannel = "borrowing_pool_quota"
+)
+
+// @group Misc
+const (
+	PrivateChannelAveragePrice   PrivateChannel = "average_price"
+	PrivateChannelFavoriteMarket PrivateChannel = "favorite_market"
+)
